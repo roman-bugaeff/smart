89812011ad1d78d2301d10c1ec3b0e4bead350f5
roman-bugaeff <rbugaeff@gmail.com>
2020-06-09 20:28:22 +0300

    add new files

---
+++ app.js
@@ -0,0 +1,18 @@
+let express = require('express');
+let app = express();
+
+app.use(express.static('public'));
+
+app.set('view engine', 'pug');
+
+app.listen(3000, () => {
+    console.log('Server work on port 3000');
+});
+
+app.get('/', function(req, res) {
+    res.render('main');
+});
+
+app.get('/about', (req, res)=>{
+    res.render('about');
+});
